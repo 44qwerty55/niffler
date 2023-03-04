3cc0e18717283bef87a8df388d7dbd0735d1b411
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2023-03-04 16:53:06 +0300

    second homework update

--- niffler-e-2-e-tests/src/test/java/niffler/model/UserModel.java
+++ niffler-e-2-e-tests/src/test/java/niffler/model/UserModel.java
@@ -1,29 +1,20 @@
 package niffler.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserModel {
 
     private String username;
     private String password;
+    private boolean isAvailable;
 
     public UserModel(String username, String password) {
         this.username = username;
         this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+        this.isAvailable = true;
     }
 
     @Override
